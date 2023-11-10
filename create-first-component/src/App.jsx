03e4c51be871be5ebad6d8c17d69161ab00aff7e
ChanduPingili <126405951+ChanduPingili@users.noreply.github.com>
2023-11-10 23:31:00 +0530

    Add files via upload

---
+++ create-first-component/src/App.jsx
@@ -0,0 +1,7 @@
+import {KgButton} from "./KgButton"; //{} is used to import named components(multiple components)
+function App(){
+  return <div><h1>Hello, it is my first react component</h1>
+    <KgButton></KgButton></div>
+}
+//its a default export
+export default App; 
---
+++ create-first-component/src/KgButton.jsx
@@ -0,0 +1,3 @@
+export function KgButton(){ //its a named export as we are not using default keyword
+  return <button>Follow me</button>
+}
